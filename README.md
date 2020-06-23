0d811de838590a9dff54c271d1cbb195a03f79e4
Vincenzo Scalcione <vincenzo99.scalcione@gmail.com>
2020-06-23 15:01:12 +0200

    Add profile page

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # flutter_instagram_clone
 
-A new Flutter project.
+A new Flutter project. This is a flutter_instagram_clone
 
 ## Getting Started
 
@@ -14,3 +14,20 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+### GUI (Graphical Interface)
+
+- [X] App with white AppBar & StatusBar
+- [X] Root page with tabs
+
+### Profile page
+
+- [ ] Scrollable page throw CustomScrollView with user profile & post
+- [ ] User profile
+- [ ] User posts
+
+### Server Linking
+
+- [ ] Profile downloading in json format
+- [ ] Models definition (profile, user, post)
+- [ ] Model creation to json base startup
--- lib/main.dart
+++ lib/main.dart
@@ -29,19 +29,127 @@ class RootPage extends StatelessWidget {
         title: Text('@billgates'),
         centerTitle: true,
       ),
+      body: ProfilePage(),
       bottomNavigationBar: BottomNavigationBar(
-        selectedIconTheme: IconThemeData(color: Colors.indigo),
-        unselectedIconTheme: IconThemeData(color: Colors.black),
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: [
-          BottomNavigationBarItem(icon: Icon(SimpleLineIcons.home), title: Text("home")),
-          BottomNavigationBarItem(icon: Icon(Feather.search), title: Text("home")),
-          BottomNavigationBarItem(icon: Icon(SimpleLineIcons.plus), title: Text("home")),
-          BottomNavigationBarItem(icon: Icon(SimpleLineIcons.heart), title: Text("home")),
-          BottomNavigationBarItem(icon: Icon(SimpleLineIcons.user), title: Text("home")),
-        ]
-      ),
+          selectedIconTheme: IconThemeData(color: Colors.indigo),
+          unselectedIconTheme: IconThemeData(color: Colors.black),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(SimpleLineIcons.home), title: Text("home")),
+            BottomNavigationBarItem(
+                icon: Icon(Feather.search), title: Text("home")),
+            BottomNavigationBarItem(
+                icon: Icon(SimpleLineIcons.plus), title: Text("home")),
+            BottomNavigationBarItem(
+                icon: Icon(SimpleLineIcons.heart), title: Text("home")),
+            BottomNavigationBarItem(
+                icon: Icon(SimpleLineIcons.user), title: Text("home")),
+          ]),
     );
   }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        profileHeader(),
+        photoGrid(),
+      ],
+    );
+  }
+
+  Widget profileHeader() => SliverToBoxAdapter(
+        child: Column(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 35,
+                        backgroundColor: Colors.grey,
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                            Text(
+                              "@thisisbillgates",
+                              style: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.w300),
+                            ),
+                            SizedBox(height: 6),
+                            MaterialButton(
+                                minWidth: double.infinity,
+                                height: 40,
+                                color: Colors.blue.shade700,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                onPressed: () {},
+                                child: Text("Segui",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 13)))
+                          ]))
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  Text("Bill Gates",
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  SizedBox(height: 3),
+                  Text("This is a fucking creator of Windozoz"),
+                  FlatButton(
+                    padding: EdgeInsets.all(0),
+                    onPressed: () {},
+                    child: Text(
+                      "gatesnot.es/2020AnnualLetter",
+                      style: TextStyle(color: Colors.indigo.shade700),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 16),
+              decoration: BoxDecoration(
+                border: Border(top: BorderSide(color: Colors.black26, width: 1)),
+              ),
+              child: Row(
+                  children: List.generate(
+                      3,
+                          (index) => Expanded(
+                              child: Container(
+                                  child: Column(children: <Widget>[
+                                    Text("139", style: TextStyle(fontWeight: FontWeight.bold)),
+                                    SizedBox(height: 3),
+                                    Text(
+                                      "post",
+                                      style: TextStyle(color: Colors.black54)
+                                    ),
+                                  ])
+                              ))
+                  )
+              )),
+          ],
+        ),
+      );
+
+  Widget photoGrid() => SliverGrid(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 1,
+          crossAxisSpacing: 1,
+          mainAxisSpacing: 1,
+        ),
+        delegate: SliverChildListDelegate([]),
+      );
+
 }
